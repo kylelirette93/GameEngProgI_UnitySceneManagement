39579c0fe473694fe5cf346f50ba815f4f67bb97
kylelirette93 <162719550+kylelirette93@users.noreply.github.com>
2025-01-30 13:42:37 -0400

    Fixed tilemap borders

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -6,11 +6,13 @@ using UnityEngine.SceneManagement;
 public class LevelManager : MonoBehaviour
 {
     public static LevelManager Instance { get; private set; }
-    string _spawnPoint;
+    // Reference to the player and the spawn point
     Transform player;
+    string _spawnPoint;
 
     private void Awake()
     {
+        // Singleton pattern
         if (Instance == null)
         {
             Instance = this;
@@ -25,22 +27,27 @@ public class LevelManager : MonoBehaviour
 
     public void ChangeScene(string sceneName, string spawnPoint)
     {
+        // Set spawn point to the one passed in by trigger.
         _spawnPoint = spawnPoint;
-        SceneManager.sceneLoaded += OnSceneLoaded;  // Listen for scene load
-        SceneManager.LoadScene(sceneName);  // Load the scene
+        // Subscribe to the sceneLoaded event
+        SceneManager.sceneLoaded += OnSceneLoaded; 
+        // Begin loading the scene.
+        SceneManager.LoadScene(sceneName);  
         
     }
 
     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
+        // Find the player object.
         player = GameObject.Find("Player").transform;
+
         // Find the spawn point object based on the name passed in
         Transform spawn = GameObject.Find(_spawnPoint).transform;
 
-        // Move the player to the spawn point
+        // Move the player to the spawn point.
         player.position = spawn.position;
 
-        // Remove the listener to avoid duplicate calls
+        // Remove the listener to avoid duplicate calls.
         SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 }
--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -7,12 +7,9 @@ public class SceneChange : MonoBehaviour
 {
     public string sceneName;
     public string spawnPoint;
-    
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         LevelManager.Instance.ChangeScene(sceneName, spawnPoint);
-    }
-
-   
-    
+    }  
 }
